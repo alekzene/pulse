9e297b532656cdb3b8aacce7c1361a332c84a811
— <alekzene.cs@gmail.com>
2024-05-30 14:09:55 +0800

    upload blade templates

---
+++ pulse/resources/views/templates/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <div class="logo">
+        <img src="images/Pulse Logo.png" alt="" />
+    </div>
+    <nav>
+        <a href="#">Help</a>
+        <a href="#">Sign In</a>
+    </nav>
+</header>
---
+++ pulse/resources/views/templates/navbar.blade.php
@@ -0,0 +1,14 @@
+<div class="sidebar">
+    <a href="#" class="profile-link">
+        <div class="profile">
+            <img src="images/image 107.png" alt="">
+            <p>Profile</p>
+        </div>
+    </a>
+    <ul>
+        <li><a href="#"><img src="images/Home Icon.png" alt=""> News Feed</a></li>
+        <li><a href="#"><img src="images/Bell Icon.png" alt=""> Notification</a></li>
+        <li><a href="#"><img src="images/Settings Icon.png" alt=""> Settings</a></li>
+        <li><a href="#"><img src="images/Help Icon.png" alt=""> Help</a></li>
+    </ul>
+</div>
