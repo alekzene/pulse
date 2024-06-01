0d6cb29a2254ae09fb6d4fe24b6b2c64ceceb84f
Zenkhria <118682206+Zenkhria@users.noreply.github.com>
2024-06-01 21:39:35 +0800

    Minor Routes Display Changes

--- pulse/routes/web.php
+++ pulse/routes/web.php
@@ -3,26 +3,37 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/login', function () {
-//     return view('login');
-// });
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/login', [AuthController::class, 
+    'showLoginForm'
+])->name('login');
+
+Route::post('/login', [AuthController::class, 
+    'login'
+]);
+
+Route::get('/create-acct', function () {
+    return view('create-acct');
+})->name('create-acct');
+
+Route::post('/create-acct', [AuthController::class, 
+    'createAccount'
+])->name('create-acct');
 
 Route::get('/help', function () {
     return view('help');
 })->name('help');
 
-Route::post('/help', [AuthController::class, 'Help'] )->name('help');
+Route::post('/help', [AuthController::class, 
+    'Help'
+])->name('help');
 
 Route::get('/feed', function () {
     return view('feed');
@@ -37,12 +48,6 @@ Route::get('/profile', function () {
 Route::get('/reset-pass', function () {
     return view('reset-pass');
 });
-
-Route::get('/create-acct', function () {
-    return view('create-acct');
-})->name('create-acct');
-
-Route::post('/create-acct', [AuthController::class, 'createAccount'])->name('create-acct');
 
 Route::get('/reset-pass-done', function () {
     return view('reset-pass-done');
