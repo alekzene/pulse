f2e8e9cd1a976ef11abedacbad68009b0fbfce11
— <alekzene.cs@gmail.com>
2024-06-02 02:57:00 +0800

    create post template

---
+++ pulse/resources/views/templates/post.blade.php
@@ -0,0 +1,16 @@
+<div class="post">
+    <div class="post-header">
+        <img src="img/user-profile-icon.png" alt="Profile Picture">
+        <div>
+            <h4>Nath ♥ Mich</h4>
+            <p>@NathanielMapagmahal25</p>
+        </div>
+    </div>
+    <div class="post-description">
+        <p>Feels Like Summer when Im with you parang Islang Pantropiko.</p>
+    </div>
+<div class="post-images">
+    <img src="img/picture-1.png" alt="">
+    <img src="img/picture-2.png" alt="">
+</div>
+</div>
