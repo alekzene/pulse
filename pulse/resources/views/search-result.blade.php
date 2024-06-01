5b613cc4f287c059ff58b238e841b49437098f9f
AqueousThalasso <arrojojomary@gmail.com>
2024-06-01 16:58:50 +0800

    Finished Search Result
    Gawa ni Jimboy

--- pulse/resources/views/search-result.blade.php
+++ pulse/resources/views/search-result.blade.php
@@ -8,33 +8,75 @@
     <link rel="stylesheet" href="{{ asset('css/search-result.css')}}">
     <link href="{{ asset('css/header.css') }}" rel="stylesheet">
     <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
+    
 </head>
 <body>
     @include('templates.header')
     @include('templates.navbar')
-    <div class="main-content">
+    <div class="search-main-content">
         <div class="feed-container">
             <p>Search result for "<b>nat nat<b>"</p>
             <div class="search-result-container">
                 <div class="profile-cards">
                     <div class="profile-card">
-                        <div class="green-background"></div>
-                            <img src="profile-pic.jpg" alt="Profile Picture">
-                            </div>
-                        </div>
-                        <div class="profile-card">
-                            <div class="green-background"></div>
+                        <div class="green-background-container">
+                            <img src="img/user-profile-icon.png" alt="">
+                            <h3>Nath❤️Mich</h3>
+                            <p>@NathanielMapagmahal25</p>
+                            <div class="icon-container">
+                                <div class="icons">
+                                    <img src="img/icons/subscribe.png" alt="?" id="icon1">
                                 </div>
-                            </div>
-                            <div class="profile-card">
-                                <div class="green-background"></div>
-                                    <img src="profile-pic.jpg" alt="Profile Picture">
+                                <div class="icons">
+                                    <img src="img/icons/follow.png" alt="?" id="icon2">
                                 </div>
-                            </div>
+                                <div class="icons">
+                                    <img src="img/icons/block.png" alt="?" id="icon3">
+                                </div>
+                            </div>  
+                            <p1>Love ko si Mich</p1>
                         </div>
                     </div>
-                </div>
-            </div>
+                    <div class="profile-card">
+                        <div class="green-background-container">
+                            <img src="img/user-profile-icon.png" alt="">
+                            <h3>Nath❤️Mich</h3>
+                            <p>@NathanielMapagmahal25</p>
+                            <div class="icon-container">
+                                <div class="icons">
+                                    <img src="img/icons/subscribe.png" alt="?" id="icon1">
+                                </div>
+                                <div class="icons">
+                                    <img src="img/icons/follow.png" alt="?" id="icon2">
+                                </div>
+                                <div class="icons">
+                                    <img src="img/icons/block.png" alt="?" id="icon3">
+                                </div>
+                            </div>  
+                            <p1>Love ko si Mich</p1>
+                        </div>
+                    </div>
+                    <div class="profile-card">
+                        <div class="green-background-container">
+                            <img src="img/user-profile-icon.png" alt="">
+                            <h3>Nath❤️Mich</h3>
+                            <p>@NathanielMapagmahal25</p>
+                            <div class="icon-container">
+                                <div class="icons">
+                                    <img src="img/icons/subscribe.png" alt="?" id="icon1">
+                                </div>
+                                <div class="icons">
+                                    <img src="img/icons/follow.png" alt="?" id="icon2">
+                                </div>
+                                <div class="icons">
+                                    <img src="img/icons/block.png" alt="?" id="icon3">
+                                </div>
+                            </div>  
+                            <p1>Love ko si Mich</p1>
+                        </div>
+                    </div>
+                 </div>
+             </div>
         </div>
     </div>
 </body>
