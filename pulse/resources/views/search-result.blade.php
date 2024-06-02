b691436a7447d0413b4f5d3f5ab7395c8148512a
— <alekzene.cs@gmail.com>
2024-06-03 02:21:34 +0800

    polish search result view

--- pulse/resources/views/search-result.blade.php
+++ pulse/resources/views/search-result.blade.php
@@ -5,77 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search | Pulse</title>
 
-    <link rel="stylesheet" href="{{ asset('css/search-result.css')}}">
+    @include('templates.font')
     <link href="{{ asset('css/header.css') }}" rel="stylesheet">
     <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
-    
+    <link href="{{ asset('css/search-result.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/profile-card.css') }}" rel="stylesheet">
 </head>
 <body>
     @include('templates.header')
     @include('templates.navbar')
-    <div class="search-main-content">
-        <div class="feed-container">
-            <p>Search result for "<b>nat nat<b>"</p>
-            <div class="search-result-container">
-                <div class="profile-cards">
-                    <div class="profile-card">
-                        <div class="green-background-container">
-                            <img src="img/user-profile-icon.png" alt="">
-                            <h3>Nath❤️Mich</h3>
-                            <p>@NathanielMapagmahal25</p>
-                            <div class="icon-container">
-                                <div class="icons">
-                                    <img src="img/icons/subscribe.png" alt="?" id="icon1">
-                                </div>
-                                <div class="icons">
-                                    <img src="img/icons/follow.png" alt="?" id="icon2">
-                                </div>
-                                <div class="icons">
-                                    <img src="img/icons/block.png" alt="?" id="icon3">
-                                </div>
-                            </div>  
-                            <p1>Love ko si Mich</p1>
-                        </div>
-                    </div>
-                    <div class="profile-card">
-                        <div class="green-background-container">
-                            <img src="img/user-profile-icon.png" alt="">
-                            <h3>Nath❤️Mich</h3>
-                            <p>@NathanielMapagmahal25</p>
-                            <div class="icon-container">
-                                <div class="icons">
-                                    <img src="img/icons/subscribe.png" alt="?" id="icon1">
-                                </div>
-                                <div class="icons">
-                                    <img src="img/icons/follow.png" alt="?" id="icon2">
-                                </div>
-                                <div class="icons">
-                                    <img src="img/icons/block.png" alt="?" id="icon3">
-                                </div>
-                            </div>  
-                            <p1>Love ko si Mich</p1>
-                        </div>
-                    </div>
-                    <div class="profile-card">
-                        <div class="green-background-container">
-                            <img src="img/user-profile-icon.png" alt="">
-                            <h3>Nath❤️Mich</h3>
-                            <p>@NathanielMapagmahal25</p>
-                            <div class="icon-container">
-                                <div class="icons">
-                                    <img src="img/icons/subscribe.png" alt="?" id="icon1">
-                                </div>
-                                <div class="icons">
-                                    <img src="img/icons/follow.png" alt="?" id="icon2">
-                                </div>
-                                <div class="icons">
-                                    <img src="img/icons/block.png" alt="?" id="icon3">
-                                </div>
-                            </div>  
-                            <p1>Love ko si Mich</p1>
-                        </div>
-                    </div>
-                 </div>
+    <div class="container">
+        <p class="search-result-label">Search results for "<b>nat nat<b>"</p>
+        <div class="content">
+            <div class="profile-cards">
+                @include('templates.profile-card')
+                @include('templates.profile-card')
+                @include('templates.profile-card')
              </div>
         </div>
     </div>
--- pulse/resources/views/templates/profile-card.blade.php
+++ pulse/resources/views/templates/profile-card.blade.php
@@ -1,9 +1,11 @@
 <div class="profile-card">
-    <img class="profile-pic" src="img/user-profile-icon.png" alt="Profile Picture">
-    <div class="user-details">
-        <h1 class="display-name">Nath ♥ Mich</h1>
-        <p class="username">@NathanielMapagmahal25</p>
-    </div>
-    <button class="add-friend-button">Add Friend</button>
-    <textarea class="bio" placeholder="Love ko si Mich <3" ></textarea>
+    {{-- <div class="green-card-section"> --}}
+        <img class="profile-pic" src="img/user-profile-icon.png" alt="Profile Picture">
+        <div class="user-details">
+            <h1 class="display-name">Nath ♥ Mich</h1>
+            <p class="username">@NathanielMapagmahal25</p>
+        </div>
+        <button class="add-friend-button">Add Friend</button>
+        <textarea class="bio" placeholder="Love ko si Mich <3" ></textarea>
+    {{-- </div> --}}
 </div>
