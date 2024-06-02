b0f3d8b618b7a7c5c23e8f3878d4c26de8f1835a
— <alekzene.cs@gmail.com>
2024-06-02 18:04:39 +0800

    create page-not-found template

--- pulse/resources/views/notification.blade.php
+++ pulse/resources/views/notification.blade.php
@@ -4,19 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Notification - Ongoing </title>
-    <link rel="stylesheet" href="{{ asset('css/notification.css')}}">
 
+    @include('templates.font')
     <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
     <link href="{{ asset('css/header.css') }}" rel="stylesheet">
-
+    <link href="{{ asset('css/notification.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/page-not-found.css') }}" rel="stylesheet">
 </head>
 <body>
     @include('templates.header')
-    @include('templates.navbar')
+    @include('templates.navbar')  
+      
     <div class="main-content">
         <div class="feed-container">
             <div class="warning-panel">
-            <h1>Oops! This page is under construction.</h1>
+                <h1>Oops! This page is under construction.</h1>
                 <p>Developers are working hard to bring you this feature.</p>
             </div>
         </div>
---
+++ pulse/resources/views/templates/page-not-found.php
@@ -0,0 +1,8 @@
+<div class="main-content">
+    <div class="feed-container">
+        <div class="warning-panel">
+        <h1>Oops! This page is under construction.</h1>
+            <p>Developers are working hard to bring you this feature.</p>
+        </div>
+    </div>
+</div>
