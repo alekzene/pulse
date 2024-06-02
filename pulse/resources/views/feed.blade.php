e05b685c4f65e0f9f374a269ad9fc0d3adddc8bf
— <alekzene.cs@gmail.com>
2024-06-02 17:11:30 +0800

    Update feed.blade.php
    integrate font into feed page

--- pulse/resources/views/feed.blade.php
+++ pulse/resources/views/feed.blade.php
@@ -5,11 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Feed</title>
 
+    @include('templates.font')
+
     <link href="{{ asset('css/header.css') }}" rel="stylesheet">
     <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
     <link href="{{ asset('css/feed.css') }}" rel="stylesheet">
     <link href="{{ asset('css/post.css') }}" rel="stylesheet">
     <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/font.css') }}" rel="stylesheet">
 </head>
 <body>
     @include('templates.header')
