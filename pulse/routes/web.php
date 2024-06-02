8ada9bbf3c56c0bc50ab6b93d0f75f1f043b3c1e
Zenkhria <118682206+Zenkhria@users.noreply.github.com>
2024-06-02 22:14:59 +0800

    Fixed Minor Bugs

--- pulse/routes/web.php
+++ pulse/routes/web.php
@@ -73,15 +73,23 @@ Route::get('/notification', function () {
 
 Route::get('/privacy', function () {
     return view('privacy');
-});
+})->name('privacy');
 
 Route::get('/privacy-change-email', function () {
     return view('privacy-change-email');
 })->name('privacy-change-email');
 
+Route::post('/update-email', [AuthController::class, 
+    'updateEmail'
+])->name('update-email');
+
 Route::get('/privacy-change-password', function () {
     return view('privacy-change-password');
 })->name('privacy-change-password');
+
+Route::post('/update-password', [AuthController::class, 
+    'updatePassword'
+])->name('update-password');
 
 Route::get('/create-post', function () {
     return view('create-post');
