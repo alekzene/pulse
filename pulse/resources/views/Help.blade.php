eeddb631400b485642aa18ef8253ce9148f1ed7b
— <alekzene.cs@gmail.com>
2024-06-03 10:41:52 +0800

    polish documentation view

--- pulse/resources/views/Help.blade.php
+++ pulse/resources/views/Help.blade.php
@@ -4,76 +4,60 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Documentation | Pulse</title>
+
+        @include('templates.font')
+        <link rel="stylesheet" href="{{ asset('css/menu.css')}}">
         <link rel="stylesheet" href="{{ asset('css/help.css')}}">
-        <link rel="stylesheet" href="{{assert('css/footer.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/footer.css')}}">
         
     </head>
     <body>
-        <header>
-            <div class="logo">
-                <img src="img/pulse-logo.png" alt="" />
+        @include('templates.menu')
+        <div class="container">
+            <div class="text">
+                <h1 class="label">Documentation</h1>
+                <p class="description">This is a social media site similar to Facebook, created by the team w3ird0s. It aims to have Create, Read, 
+                    Update, and Delete (CRUD) functionality, which the developers are required to implement.  </p>
             </div>
-            <nav>
-                <a href="help">Help</a>
-                <a href="{{ route('login') }}">Sign In</a>
-            </nav>
-        </header>
-        <main>
             <div class="content">
-                <div class="text">
-                    <h1>Documentation</h1>
-                    <p>This is a social media site similar to Facebook, created by the team w3ird0s. It aims to have Create, Read, 
-                        Update, and Delete (CRUD) functionality, which the developers are required to implement.  </p>
+                <h1 class="help-title"> How Does it Work?</h1>
+                <div class="first-row">
+                    <div class="container1">
+                        <h1>1.</h1>
+                            <p><strong>Posting</strong>. Whether it's a 
+                                <br>quick update or a 
+                                <br>detailed article, express 
+                                <br>yourself effortlessly.
+                            </p>
+                    </div>
+    
+                    <div class="container2">
+                        <h1>2.</h1>
+                            <p><strong>Connect</strong>. Discover like-
+                                <br>minded people using our 
+                                <br>powerful search tool.</p>
+                    </div>
                 </div>
-            </div>
-        </main>
-        <div class="green-container">
-            <h1> How Does it Work?</h1>
-                <div class="container1">
-                    <h1>1.</h1>
-                        <p><strong>Posting</strong>. Whether it's a 
-                            <br>quick update or a 
-                            <br>detailed article, express 
-                            <br>yourself effortlessly.
-                        </p>
+                <div class="second-row">
+                    <div class="container3">
+                        <h1>3.</h1>
+                            <p><strong>Customize</strong>. Make your 
+                                <br>profile uniquely yours. 
+                                <br>Express yourself through 
+                                <br>your bio, and showcase 
+                                <br>your individuality.</p>
+                    </div>
+                    <div class="container4">
+                        <h1>4.</h1>
+                            <p><strong>Engage</strong>. Converse and 
+                                <br>connect on a deeper level 
+                                <br>with our dynamic 
+                                <br>engagement tools.</p>
+                    </div>
                 </div>
-
-                <div class="container2">
-                    <h1>2.</h1>
-                        <p><strong>Connect</strong>. Discover like-
-                            <br>minded people using our 
-                            <br>powerful search tool.</p>
-                </div>
-
-                <div class="container3">
-                    <h1>3.</h1>
-                        <p><strong>Customize</strong>. Make your 
-                            <br>profile uniquely yours. 
-                            <br>Express yourself through 
-                            <br>your bio, and showcase 
-                            <br>your individuality.</p>
-                </div>
-                <div class="container4">
-                    <h1>4.</h1>
-                        <p><strong>Engage</strong>. Converse and 
-                            <br>connect on a deeper level 
-                            <br>with our dynamic 
-                            <br>engagement tools.</p>
             </div>
         </div>
-        <div class="text1">
-            <div class="head">
-                <p><strong>© 2024 Pulse. All rights reserved.</strong></p>
-            </div>
-            <div class="mean">
-                <p>Contact Us: pulse@application_development.com
-                    <br>Address: Centro Escolar University - Makati Legazpi Village Campus
-                    <br>
-                    <br>This website is designed with love for our planet and to be user friendly. 
-                        Let's work together for a greener future!
-                </p>
-            </div>
-        </div>
+        @include('templates.footer')
     </body>
 </html>
 
