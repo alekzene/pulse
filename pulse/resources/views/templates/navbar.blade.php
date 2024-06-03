c3cfa72f46736c54f06938b9cfc407285fea69ee
— <alekzene.cs@gmail.com>
2024-06-03 11:17:31 +0800

    minor change navbar

--- pulse/resources/views/templates/navbar.blade.php
+++ pulse/resources/views/templates/navbar.blade.php
@@ -9,7 +9,7 @@
     </a>
     <ul>
         <li><a href="feed"><img src="img/icons/home.png" alt=""> News Feed</a></li>
-        <li><a href="notification"><img src="img/icons/bell.png" alt=""> Notification</a></li>
+        <li><a href="notification"><img src="img/icons/bell.png" alt=""> Notifications</a></li>
         <li><a href="privacy"><img src="img/icons/settings.png" alt=""> Settings</a></li>
         <li><a href="help-2"><img src="img/icons/help.png" alt=""> Help</a></li>
         <li><a href="login"><img src="img/icons/log-out.png" alt=""> Log Out</a></li>
